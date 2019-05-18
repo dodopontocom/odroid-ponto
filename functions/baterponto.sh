dd9bd5e700f253d91f94f3496db8ec74f4af66d1
rodolfotiago <rodolfotiago@gmail.com>
2019-05-18 19:13:19 -0300

    saida 2

--- functions/baterponto.sh
+++ functions/baterponto.sh
@@ -10,7 +10,7 @@ eight_hours_in_seconds=28800
 four_hours_in_seconds=$(echo $((eight_hours_in_seconds/2)))
 
 baterponto.entrada() {
-	local work_day_start_sec reply_user estimate log file message flag flag2 weekday day verify verify2
+	local work_day_start_sec reply_user estimate log file message flag flag2 weekday day verify verify_saida
 	weekday=$(date +%a)
 	day=$(date +%Y%m%d)
 	file=${day}.csv
@@ -94,16 +94,25 @@ baterponto.volta() {
 	fi
 }
 baterponto.saida() {
-	local leave_day_sec work_day_start_sec reply_user estimate log file message flag weekday day verify
+	local leave_day_sec work_day_start_sec reply_user estimate log file message flag flag2 weekday day verify verify_segunda_entrada
 	weekday=$(date +%a)
 	day=$(date +%Y%m%d)
 	file=${day}.csv
 	flag=saida
+	flag2=2saida
 	log=${BASEDIR}/logs/${message_from_id}
-	
-	if [[ ! -f $log/$file ]]; then
+	verify_segunda_entrada=$(cat $log/$file | grep $day | grep ,2entrada | cut -d',' -f4)
+
+	if [[ ! -f $log/$file ]] && [[ -z $verify_segunda_entrada ]]; then
 		message="Entrada ainda nao registrada. Registre a Entrada primeiro"
 		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	elif [[ ! -z $verify_segunda_entrada ]]; then
+		message="Registrando a Segunda saida -> "
+		leave_day_sec="$(date --date="now" +%s)"
+		reply_user=$(date --date="now" +'%H:%M')
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} ${reply_user})" --parse_mode markdown
+
+		echo "$day,$weekday,$leave_day_sec,$reply_user,$flag2" >> $log/$file
 	else
 		verify=$(cat $log/$file | grep $day | grep $flag | cut -d',' -f4)
 		if [[ ! -z $verify ]]; then
