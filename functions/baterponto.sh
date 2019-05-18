8a3e1278cc795c0a63001aa37273550f4f69035b
rodolfotiago <rodolfotiago@gmail.com>
2019-05-18 18:24:10 -0300

    new function almoco

--- functions/baterponto.sh
+++ functions/baterponto.sh
@@ -32,6 +32,32 @@ baterponto.entrada() {
 		echo "$day,$weekday,$work_day_start_sec,$reply_user,$flag" >> $log/$file
 	fi
 }
+baterponto.almoco() {
+	local go_lunch_sec work_day_start_sec reply_user estimate log file message flag weekday day verify
+	weekday=$(date +%a)
+	day=$(date +%Y%m%d)
+	file=${day}.csv
+	flag=almoco
+	log=${BASEDIR}/logs/${message_from_id}
+	
+	if [[ ! -f $log/$file ]];
+		message="Entrada ainda nao registrada. Registre a Entrada primeiro"
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	else
+		verify=$(cat $log/$file | grep $day | grep $flag | cut -d',' -f4)
+		if [[ ! -z $verify ]]; then
+			message="Saida para almoco foi registrada as ${verify}"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		else
+			message="Registrando horário de almoco -> "
+			go_lunch_sec="$(date --date="now" +%s)"
+			reply_user=$(date --date="now" +'%H:%M')
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} ${reply_user})" --parse_mode markdown
+
+			echo "$day,$weekday,$go_lunch_sec,$reply_user,$flag" >> $log/$file
+		fi
+	fi
+}
 
 ponto.calc() {
 	#triggered_time will be $(date +%s)
