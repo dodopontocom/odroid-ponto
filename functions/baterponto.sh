718c2694040f4a05fe67eeb6d0a73a81d319473e
rodolfotiago <rodolfotiago@gmail.com>
2019-05-18 18:35:32 -0300

    mais melhor

--- functions/baterponto.sh
+++ functions/baterponto.sh
@@ -58,6 +58,58 @@ baterponto.almoco() {
 		fi
 	fi
 }
+baterponto.volta() {
+	local back_lunch_sec work_day_start_sec reply_user estimate log file message flag weekday day verify
+	weekday=$(date +%a)
+	day=$(date +%Y%m%d)
+	file=${day}.csv
+	flag=volta
+	log=${BASEDIR}/logs/${message_from_id}
+	
+	if [[ ! -f $log/$file ]]; then
+		message="Entrada ainda nao registrada. Registre a Entrada primeiro"
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	else
+		verify=$(cat $log/$file | grep $day | grep $flag | cut -d',' -f4)
+		if [[ ! -z $verify ]]; then
+			message="Volta do almoco foi registrada as ${verify}"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		else
+			message="Registrando a volta do almoco -> "
+			back_lunch_sec="$(date --date="now" +%s)"
+			reply_user=$(date --date="now" +'%H:%M')
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} ${reply_user})" --parse_mode markdown
+
+			echo "$day,$weekday,$back_lunch_sec,$reply_user,$flag" >> $log/$file
+		fi
+	fi
+}
+baterponto.saida() {
+	local leave_day_sec work_day_start_sec reply_user estimate log file message flag weekday day verify
+	weekday=$(date +%a)
+	day=$(date +%Y%m%d)
+	file=${day}.csv
+	flag=saida
+	log=${BASEDIR}/logs/${message_from_id}
+	
+	if [[ ! -f $log/$file ]]; then
+		message="Entrada ainda nao registrada. Registre a Entrada primeiro"
+		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+	else
+		verify=$(cat $log/$file | grep $day | grep $flag | cut -d',' -f4)
+		if [[ ! -z $verify ]]; then
+			message="Saida foi registrada as ${verify}"
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+		else
+			message="Registrando a saida -> "
+			leave_day_sec="$(date --date="now" +%s)"
+			reply_user=$(date --date="now" +'%H:%M')
+			ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message} ${reply_user})" --parse_mode markdown
+
+			echo "$day,$weekday,$leave_day_sec,$reply_user,$flag" >> $log/$file
+		fi
+	fi
+}
 
 ponto.calc() {
 	#triggered_time will be $(date +%s)
