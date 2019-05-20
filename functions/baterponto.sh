dc0329b1fb249741a8b027235562d4e2af3dc641
rono Rodolfo Neto <rodolfo.neto@gft.com>
2019-05-20 21:19:27 +0000

    adding lunchalert function

--- functions/baterponto.sh
+++ functions/baterponto.sh
@@ -161,14 +161,13 @@ baterponto.saida() {
 	send_summary=$(cat $log/$file | grep $day | grep ,$flag)
 	send_summary2=$(cat $log/$file | grep $day | grep ,$flag2)
 	if [[ ! -z $send_summary ]] || [[ ! -z $send_summary2 ]]; then
-		#call sum function
-		ponto.calc $log/$file
+		baterponto.calc $log/$file
 		message="$(cat $log/$file)"
 		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
 	fi
 }
 
-ponto.calc() {
+baterponto.calc() {
 	local message file fsize time_spent_at_work go_lunch_sec work_day_start_sec back_lunch_sec leave_day_sec first_time_sum day_closure
 	file=$1
 	fsize=$(cat $file | wc -l)
@@ -208,56 +207,26 @@ ponto.calc() {
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
 			;;
 	esac
-	eight_hours_in_seconds_consider_lunch=32400
-	estimate_leave_hour=$(date -d "now + \
-	  $eight_hours_in_seconds_consider_lunch seconds" +'%H:%M')
-	work_day_start_sec="$(date --date="now" +%s)"
-	#go_lunch_sec="$(date --date="now + 14400 seconds" +%s)"
-	go_lunch_sec="$(date --date="now" +%s)"
-	#back_lunch_sec="$(date --date="now + 18000 seconds" +%s)"
-	back_lunch_sec="$(date --date="now" +%s)"
-	time_in_lunch=$(echo $(((back_lunch_sec-go_lunch_sec))))
-	first_time_sum=$(echo $(((go_lunch_sec-work_day_start_sec))))
-	estimate_after_lunch=$(date -d "now + \
-	  $(echo $(((eight_hours_in_seconds+time_in_lunch)))) seconds" +'%H:%M')
-	#leave_day_sec="$(date --date="now + 61200 seconds" +%s)"
-	leave_day_sec="$(date --date="now" +%s)"
-	day_closure=$(echo $(((leave_day_sec-back_lunch_sec)-first_time_sum)))
-	time_spent_at_work=$(echo $(date -d "00:00 today + $day_closure seconds" +'%H:%M'))
-
-	echo $estimate_leave_hour
-	echo $estimate_after_lunch
-	echo $day_closure
-	echo $time_spent_at_work
 }
 
-baterponto.apply() {
-  local message opt array random_file_name
-  opt=$1
-  array=(${opt})
-  array[0]="/baterponto"
-  opt=${array[@]:1}
-  case ${opt} in
-		'start')
-            message="Bom trabalho"
-        		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-			;;
-		'lunch')
-        		message="Bom almoço"
-        		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-			;;
-		'back')
-        		message="Bom trabalho"
-        		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-			;;
-		'bye')
-        		message="Bom retorno"
-        		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-			;;
-		'')
-            		message="opa"
-        		ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
-        
-      			;;
-  	esac
+baterponto.lunchAlert() {
+	#return alert to the user when 1 hour of lunch is expiring
+	local complete_one_hour_in_sec file day fift_min_in_sec five_min_in_sec dt_now user_id message
+	five_min_in_sec=300
+	fift_min_in_sec=900
+	file=$1
+	user_id=$(echo $file | rev | cut -d'/' -f2 | rev)
+	
+	complete_one_hour_in_sec=$(echo $(($(cat $file | grep $day | grep ,almoco | cut -d',' -f3)+3600)))
+	
+	dt_now=$(date --date="now" +'%s')
+	if [[ $((complete_one_hour_in_sec-dt_now)) -lt $fift_min_in_sec ]]; then
+		message="Alerta, faltam 15 minutos para completar 1 hora de almoço..."
+				ShellBot.sendMessage --chat_id ${user_id} --text "$(echo -e ${message})" --parse_mode markdown
+	fi
+	if [[ $((complete_one_hour_in_sec-dt_now)) -lt $five_min_in_sec ]]; then
+		message="Alerta, faltam apenas 5 minutos para completar 1 hora de almoço..."
+				ShellBot.sendMessage --chat_id ${user_id} --text "$(echo -e ${message})" --parse_mode markdown
+	fi
+
 }
