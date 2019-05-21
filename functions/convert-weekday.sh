38163810bc4a5348e7db0f951f0ba6b1ef8a974e
Rodolfo Neto <rodolfotiago@gmail.com>
2019-05-21 08:57:57 -0300

    Create convert-weekday.sh

---
+++ functions/convert-weekday.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#e.g. : convert.weekdayPtbr $(date +%u)
+local segunda terca quarta quinta sexta sabado domingo
+convert.weekdayPtbr() {
+
+  local day
+  day=$1
+  case ${day} in
+      '1' ) echo "Segunda feira"
+        ;;
+      '2' ) echo "Terça feira"
+        ;;
+      '3' ) echo "Quarta feira"
+        ;;
+      '4' ) echo "Quinta feira"
+        ;;
+      '5' ) echo "Sexta feira"
+        ;;
+      '6' ) echo "Sábado"
+        ;;
+      '7' ) echo "Domingo"
+        ;;
+      * ) echo "Error"
+        ;;
+  esac
+
+}
