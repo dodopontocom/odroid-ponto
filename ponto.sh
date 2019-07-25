64eaf4e3346ee6c2247559a6abf968c4fab8b38d
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-25 11:35:36 -0300

    Update ponto.sh

--- ponto.sh
+++ ponto.sh
@@ -7,6 +7,7 @@ BASEDIR=$(dirname $0)
 source ${BASEDIR}/ShellBot.sh
 source ${BASEDIR}/functions/start.sh
 source ${BASEDIR}/functions/baterponto.sh
+source ${BASEDIR}/functions/send_notification.sh
 
 ######################################################################################
 #source <(cat ${BASEDIR}/functions/*.sh)
@@ -18,9 +19,8 @@ logs=${BASEDIR}/logs
 # Token do bot
 bot_token=$(cat ${BASEDIR}/.token)
 
-VERSION=$(cd $BASEDIR ; git branch | grep -E "\*" ; cd -)
+VERSION=$(cd ${BASEDIR} ; git branch | grep -E "\*" ; cd -)
 BT_VERSION=${VERSION:2}
-echo "----------------------------- $BT_VERSION"
 
 # Inicializando o bot
 ShellBot.init --token "$bot_token" --monitor --flush
@@ -50,31 +50,6 @@ ch_keyboard1="$(ShellBot.ReplyKeyboardMarkup --button 'btn_opcoes' --one_time_ke
 #❌
 #✅
 ############### keyboard para o comando trip #######################################
-botao2=''
-ShellBot.InlineKeyboardButton --button 'botao2' --line 1 --text 'Listar Todos' --callback_data 'btn_trip_list'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 2 --text 'Listar ✅' --callback_data 'btn_trip_done'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 2 --text 'Listar ❌' --callback_data 'btn_trip_pending'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 4 --text 'Listar Passagens ✅' --callback_data 'btn_trip_passagensV'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 4 --text 'Listar Passagens ❌' --callback_data 'btn_trip_passagensX'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 5 --text 'Listar Trem ✅' --callback_data 'btn_trip_tremV'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 5 --text 'Listar Trem ❌' --callback_data 'btn_trip_tremX'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 6 --text 'Listar Compras ✅' --callback_data 'btn_trip_comprarV'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 6 --text 'Listar Compras ❌' --callback_data 'btn_trip_comprarX'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 7 --text 'Listar Outros ✅' --callback_data 'btn_trip_outrosV'
-ShellBot.InlineKeyboardButton --button 'botao2' --line 7 --text 'Listar Outros ❌' --callback_data 'btn_trip_outrosX'
-#ShellBot.regHandleFunction --function list.all --callback_data btn_trip_list
-#ShellBot.regHandleFunction --function list.done --callback_data btn_trip_done
-#ShellBot.regHandleFunction --function list.pending --callback_data btn_trip_pending
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_passagensV
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_passagensX
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_tremV
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_tremX
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_comprarV
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_comprarX
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_outrosV
-#ShellBot.regHandleFunction --function list.search --callback_data btn_trip_outrosX
-keyboard_trip_checklist="$(ShellBot.InlineKeyboardMarkup -b 'botao2')"
-#######################################################################################
 
 while :
 do
@@ -104,6 +79,11 @@ do
 									--parse_mode markdown
 				
 				start.sendGreetings
+				
+				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Comece me enviando um \`Oi\`" \
+									--parse_mode markdown
+			fi
+			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/avisos" )" ]]; then
 				
 				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Comece me enviando um \`Oi\`" \
 									--parse_mode markdown
