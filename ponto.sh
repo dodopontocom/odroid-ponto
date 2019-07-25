1d47aa9d4ab18036dcb4d18a03636eb3f904fef9
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-25 12:06:16 -0300

    Update ponto.sh

--- ponto.sh
+++ ponto.sh
@@ -85,14 +85,10 @@ do
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/avisos" )" ]]; then
 				
-				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Agora enviarei avisos amigáveis sem ser chato 🙃 . Para desligar é só enviar /avisoOff" \
-									--parse_mode markdown
 				avisos.on
 			fi
 			if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/avisoOff" )" ]]; then
 				
-				ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "Yes, aviso off 🙃 . Para ligar novamente é só enviar /avisos" \
-									--parse_mode markdown
 				avisos.off
 			fi
 		else
