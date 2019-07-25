2b82dd2be3761f2c0e1a9dd52a5e5f52d9180aaf
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-25 11:47:20 -0300

    Update send_notification.sh

--- functions/send_notification.sh
+++ functions/send_notification.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-IDS=($(ls -1 ${BASEDIR}/logs))
-IDS=(11504381 135810713 184152399 21025096 232486786 362003074 449542698 736298753 815573690 831690845 882808182 989422706)
-TOKEN=$1
+#IDS=($(ls -1 ${BASEDIR}/logs))
+#IDS=(11504381 135810713 184152399 21025096 232486786 362003074 449542698 736298753 815573690 831690845 882808182 989422706)
 
-for i in ${IDS[@]}; do
+avisos.on() {
+  local message log
+  
+  log=${BASEDIR}/logs/${message_from_id}
+  message="avisos on"
+  
+  mkdir ${log}/avisos.alert
+  
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+}
+
+avisos.off() {
+  local message log
+  
+  log=${BASEDIR}/logs/${message_from_id}
+  message="avisos off"
+  
+  rm -fv ${log}/avisos.alert
+  
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+}
+
+avisos.send() {
+  local message
+  
   message="Olá , tudo bem?\n"
   message+="Passando por aqui para perguntar se você lembrou de bater o ponto hoje!? 🤔\n"
-  message+="/start"
-    curl -X POST \
-    -d chat_id=$i \
-    -d text="$(echo -e ${message})" \
-    https://api.telegram.org/bot${TOKEN}/sendMessage
-done
+  message+="Caso não queira mais avisos /avisosOff"
+
+  ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text "$(echo -e ${message})" --parse_mode markdown
+}
