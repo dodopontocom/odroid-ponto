125e75894903114b73e5043682ec0e4c0e2154ef
Rodolfo Neto <rodolfotiago@gmail.com>
2019-07-31 18:09:41 -0300

    Create broadcast_message.sh

---
+++ broadcast_message.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+
+token=$(tail -1 ${BASEDIR}/.token)
+IDS=($(ls -1 ${BASEDIR}/logs))
+message=$1
+
+for i in ${IDS[@]}; do
+  curl -s -X POST -d chat_id=${i} -d text="$(echo -e ${message})" https://api.telegram.org/bot${token}/sendMessage
+done
